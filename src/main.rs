c7b1b948e2d75a0f43bb92d0bb88fbea8b5f4e37
albysmith <48844001+albysmith@users.noreply.github.com>
2019-10-03 10:36:01 -0500

    start

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,33 @@
+use rand::seq::SliceRandom;
+use rand::Rng;
+use std::borrow::Borrow;
+use std::collections::HashMap;
+use std::fs;
+use std::fs::File;
+use std::io::Write;
+#[macro_use]
+extern crate serde;
+extern crate serde_xml_rs;
+
+#[derive(Deserialize, Debug, Default, Clone)]
+struct Toml {
+    config: Config,
+
+}
+
+#[derive(Deserialize, Debug, Default, Clone)]
+struct Config {
+
+}
+
+
+/*
+
+To make a region:
+-
+
+*/ 
+
 fn main() {
     println!("Hello, world!");
 }
